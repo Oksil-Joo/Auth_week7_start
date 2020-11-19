0f22e485231e29ec587b94cc941271ac2516f1eb
oksil joo <oksiljoo@oksilui-MacBook-Pro.local>
2020-11-19 15:58:23 -0500

    adding python script

---
+++ Variables_Python.py
@@ -0,0 +1,23 @@
+print("This is my first Pythone script! Exciting")
+
+name = "Travor"
+age = "old"
+eyecolor = "blue"
+haircolor = "reddish - what's left, anyway"
+
+
+
+print("Name is currently " + name)
+
+name = input("What is YOUR name? ")
+age = 22
+
+print("your name is now; " + name + "and your age is " + str(age))
+
+counter = 0
+
+while (counter < 8):
+	counter += 1
+	print("counter is now at;" + str(counter))
+
+	print("counter is greater than 8, our loop is done")
